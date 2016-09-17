acf924b18f6c43f4c9308c5a259fb9f385fc6432
Archana <archana@SidMeister-MBP.local>
2016-09-17 16:31:42 -0700

    fixed untracked files

--- src/Stack/EvaluatePostfix.java
+++ src/Stack/EvaluatePostfix.java
@@ -24,6 +24,7 @@ class Calculation
 			 if(ch>='0'&& ch<='9')
 			 {
 				 //its a operand so push to the stack
+				 System.out.println("test");
 				 stack.push((int)ch -'0');//convert char to int
 			 }
 			 else
