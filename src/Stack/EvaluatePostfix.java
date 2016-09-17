41b0c49fd1769500844ede778baad995bb363e67
Archana <archana@SidMeister-MBP.local>
2016-09-17 15:58:42 -0700

    changes in EvaluatePostfix.java

--- src/Stack/EvaluatePostfix.java
+++ src/Stack/EvaluatePostfix.java
@@ -2,7 +2,57 @@ package Stack;
 
 import java.util.Scanner;
 import java.util.Stack;
-//this code need rework 
+
+class Calculation
+{
+	private Stack<Integer> stack=new Stack<Integer>();
+	private String input;
+	//private String output="";
+	
+	public Calculation(String s)
+	{
+		this.input=s;
+	}
+	public Integer evaluatePostfixExpression() {
+
+		char ch;
+		 int num1, num2,output; 
+		 //scan from left to right
+		 for(int i=0;i<input.length();i++)
+		 {
+			 ch=input.charAt(i);
+			 if(ch>='0'&& ch<='9')
+			 {
+				 //its a operand so push to the stack
+				 stack.push((int)ch -'0');//convert char to int
+			 }
+			 else
+			 {	//pop two operands to perform operation
+				 num1=stack.pop(); 
+				 num2=stack.pop();
+				 switch(ch)
+				 {
+				 case '+':
+					 output = num1 +num2;
+					 break;
+				 case '-':
+					 output = num1 -num2;
+					 break;
+				 case '*':
+					 output = num1 *num2;
+					 break;
+				 case '/':
+					 output = num1 /num2;
+					 break;
+				default:
+					output =0;
+				 }//end switch
+				 stack.push(output);
+			 }//end else
+		 }//end for
+		 return stack.pop();
+	}
+}
 public class EvaluatePostfix {
 
 	
@@ -10,48 +60,12 @@ public class EvaluatePostfix {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter Expression");
 		 String str = in.nextLine();
-		
-		evaluatePostfixExpression(str);
-		
-		 
+		 Calculation cal=new Calculation(str);
+		Integer result = cal.evaluatePostfixExpression();
+		System.out.println("Postfix Expression result is: "+result);
 	     in.close();
 	     System.out.println("test");
 
 	}
 	
-	private static void evaluatePostfixExpression(String str) {
-
-		//create stack object
-		 Stack<Character> stack =new Stack<Character>();
-		 
-		 //scan from left to right
-		 for(int i=0;i<str.length();i++)
-		 {
-			 char ch=str.charAt(i);
-			 if(ch>='0'&& ch<='9')
-			 {
-				 //its a operand so push to the stack
-				 stack.push(ch);
-			 }
-			 else
-			 {	//pop two operands to perform operation
-				 int op1=(int)stack.pop(); 
-				 int op2=(int)stack.pop();
-				 char result=(char)perform(ch,op1,op2);
-				stack.push(result); 
-			 }
-		 }
-		 char top =stack.pop();
-		 System.out.println("Output is  "+ top);
-	}
-
-	private static int perform(Character s, int a, int b) {
-		if(s.equals(new Character('*'))) return a * b;
-		else if(s.equals(new Character('/'))) return a / b;
-		else if(s.equals(new Character('-'))) return a - b;
-		else if(s.equals(new Character('+'))) return a +b;
-		else return 0;
-		
-	}
-
 }
