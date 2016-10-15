d570bde0a1b52ea479443a79d0aa9fbb7722153e
Archana <archana@SidMeister-MBP.local>
2016-10-14 20:26:01 -0700

    added new class to find next inorder successor in a tree

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -5,10 +5,11 @@ public class Node {
 		Integer data;
 		Node left;
 		Node right;
+		Node parent;
 		Node(Integer data)
 		{
 			this.data=data;
-			left=right=null;
+			left=right=parent=null;
 		}
 	public void displayNode(){
 		System.out.println("{"+data+"}");
