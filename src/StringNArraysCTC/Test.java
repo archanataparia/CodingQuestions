23125a53ea2054c2445ce416c63caf56e8b9791a
Archana <archana@SidMeister-MBP.local>
2016-11-28 22:29:40 -0800

    new programs added in Queue and stack  package

--- src/StringNArraysCTC/Test.java
+++ src/StringNArraysCTC/Test.java
@@ -2,63 +2,44 @@ package StringNArraysCTC;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.io.*;
+import java.util.Stack;
+
+
+
+
 public class Test {
-   Queue<Integer> q1 = new LinkedList<Integer>();
-	    Queue<Integer> q2 = new LinkedList<Integer>();
-
-	    public int pop() {
-	        if (q1.peek() == null) {
-	            System.out.println("The stack is empty, nothing to return");
-	            int i = 0;
-	            return i;
-	        } else {
-	            int pop = q1.remove();
-	            return pop;
-	        }
-	    }
-
-	    public void push(int data) {
-
-	        if (q1.peek() == null) {
-	            q1.add(data);
-	        } else {
-	            for (int i = q1.size(); i > 0; i--) {
-	                q2.add(q1.remove());
-	            }
-	            q1.add(data);
-	            for (int j = q2.size(); j > 0; j--) {
-	                q1.add(q2.remove());
-	            }
-
-	        }
-	    }
-
+	Stack<Integer> st=new Stack<Integer>();
 	    public static void main(String[] args) {
-	       Test s1 = new Test();
-	        //       Stack s1 = new Stack();
-	        s1.push(1);
-	        s1.push(2);
-	        s1.push(3);
-	        s1.push(4);
-	        s1.push(5);
-	        s1.push(6);
-	        s1.push(7);
-	        s1.push(8);
-	        s1.push(9);
-	        s1.push(10);
-	        // s1.push(6);
-	        System.out.println("1st = " + s1.pop());
-	        System.out.println("2nd = " + s1.pop());
-	        System.out.println("3rd = " + s1.pop());
-	        System.out.println("4th = " + s1.pop());
-	        System.out.println("5th = " + s1.pop());
-	        System.out.println("6th = " + s1.pop());
-	        System.out.println("7th = " + s1.pop());
-	        System.out.println("8th = " + s1.pop());
-	        System.out.println("9th = " + s1.pop());
-	        System.out.println("10th= " + s1.pop());
+	    	 Test sq=new  Test();
+				sq.st.push(1);sq.st.push(6);sq.st.push(3);sq.st.push(4);sq.st.push(5);sq.st.push(6);
+				
+				System.out.println(sq.checkStackPair());	
+
 	    }
+		private boolean checkStackPair() {
+			Queue<Integer> q1=new LinkedList<Integer>();
+			boolean flag=true;
+			
+			while(!st.isEmpty())
+				q1.add(st.pop());
+			while(!q1.isEmpty())
+				st.push(q1.remove());
+			
+			while(!st.isEmpty())
+			{
+				int n1=st.pop();
+				q1.add(n1);
+				if(!st.isEmpty())
+				{
+					int n2=st.pop();
+					q1.add(n2);
+					if(Math.abs(n1-n2)!=1) flag=false;
+				}
+			}
+			while(!q1.isEmpty())
+				st.push(q1.remove());
+			return flag;
+		}
 	
 	
 }
