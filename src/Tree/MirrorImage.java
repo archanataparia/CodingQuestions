1889e9c480813445075198ca68c51e4b02239ba1
Archana <archana@SidMeister-MBP.local>
2016-12-18 16:34:47 -0800

    new class added

--- src/Tree/MirrorImage.java
+++ src/Tree/MirrorImage.java
@@ -16,11 +16,22 @@ public class MirrorImage {
 		tree.displayTree();
 		System.out.println();
 		System.out.println("mirror image");
+		
 		tree.root = obj.mirrorImage(tree.root);
 		tree.displayTree();
+		
+		Tree tree1=new Tree();
+		tree1.root=new Node(1);
+		tree1.root.left=new Node(3);
+		tree1.root.right=new Node(2);
+		tree1.root.right.right=new Node(4);
+		tree1.root.right.left=new Node(5);
+		Node root2 = obj.mirrorImage(tree.root);
+		boolean result=obj.mirrorImage(tree.root, tree1.root);
+		System.out.println("result "+result);
 
 	}
-
+//create mirror image of given tree
 	private Node mirrorImage(Node root) {
 		if(root==null) return null;
 		mirrorImage(root.left);
@@ -33,5 +44,15 @@ public class MirrorImage {
 		
 		return root;
 	}
-
+//compare two tree if they are mirror image or not
+	private boolean mirrorImage(Node root1, Node root2) 
+	{
+		if(root1==null && root2==null) return true;
+		if(root1==null || root2==null) return false;
+		if(root1.data!=root2.data) 
+		return false;
+		
+		else 
+			return (mirrorImage(root1.left, root2.right) && mirrorImage(root1.right, root2.left));
+	}
 }
